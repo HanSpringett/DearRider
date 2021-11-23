5cd750539c5a93afdcee87fd1db43b9c51e83136
cristianlemeni <37481559+CristianLemeni@users.noreply.github.com>
2021-11-23 16:48:41 +0200

    v 10

--- ThreeScene.js
+++ ThreeScene.js
@@ -56,7 +56,7 @@ export default class threeScene {
         this.spinAnim
         this.rotateCoords = { x: 0, y: 0, z: 0 }
         this.sceneAssets = [
-            './assets/building_shadows_v10.gltf',
+            './assets/building_shadows_v11.gltf',
             'https://raw.githubusercontent.com/HanSpringett/DearRider_Webflow_embedtest/main/assets/Cubes.gltf',
             'https://raw.githubusercontent.com/HanSpringett/DearRider_Webflow_embedtest/main/assets/DearRider_1977.gltf',
             'https://raw.githubusercontent.com/HanSpringett/DearRider_Webflow_embedtest/main/assets/1983.gltf',
